86e9e23a60555bd2568afd5cc9e4700ebc1f9486
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2022-03-21 19:59:28 +0000

    Aula 21/03

--- TabuadaFor.java
+++ TabuadaFor.java
@@ -7,7 +7,7 @@ public class TabuadaFor {
         System.out.println("Digite o número");
         int tabuada=sc.nextInt();
         for (int i=0; i< idades.length; i++){
-            idades[i]= i*2;
+            idades[i]=tabuada*i;
         }
 
         for(int i: idades){
